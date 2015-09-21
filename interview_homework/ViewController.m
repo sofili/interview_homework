36aafb4b41eace59ab4e443d10d2aed708405aee
Sophia Lee <sleet@allstate.com>
2015-09-20 21:21:55 -0500

    milestone one
    Asynchronously fetch and print (NSLog) the response data received from
    the API endpoint described above.

--- interview_homework/ViewController.m
+++ interview_homework/ViewController.m
@@ -17,11 +17,34 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self fetchJSON];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)fetchJSON {
+    
+    
+    NSString *urlAsString = @"https://www.guidebook.com/service/v2/upcomingGuides/";
+    NSURL *url = [[NSURL alloc] initWithString:urlAsString];
+    NSLog(@"%@", urlAsString);
+    
+    NSURLSession *session = [NSURLSession sharedSession];
+
+    NSURLSessionDataTask *task = [session dataTaskWithURL:url
+                                            completionHandler:
+                                  ^(NSData *data, NSURLResponse *response, NSError *connectionError) {
+                                      
+                                      NSLog(@"HTTPS Response: %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
+                                      
+                                  }];
+    
+    [task resume];
+
 }
 
 @end
